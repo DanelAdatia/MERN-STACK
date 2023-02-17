aa1dd7aa5de270d8defe1b8ca375b43b2c3f7190
Danel Adatia <daneladatia@gmail.com>
2023-02-17 12:35:01 +0530

    Learning NodeJs and Demo of ReactJs and NodeJs

---
+++ server/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+
+/* data Imports */
+
+import User from "./models/User.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+
+/* CONFIGURATION  */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 9000;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* Only add data one time */
+    // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
---
+++ server/readme.md
@@ -0,0 +1,4 @@
+# Information about how to create this app
+
+1. npm init -y
+2. npm i express body-parser cors dotenv helmet morgan mongoose nodemon
